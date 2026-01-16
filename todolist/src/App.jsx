18907da2194e7199ae638d7b8c51c26ce74ed2f6
shubhg17 <shubamghai116@gmail.com>
2026-01-17 00:33:05 +0530

    Created Project Structure

---
+++ todolist/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <h1 className="heading">TODO LIST REACT APP</h1>
+    </>
+  )
+}
+
+export default App
---
+++ todolist/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from  "react"
+
+// function Card() {
+//    return (
+//       <>
+//       </>
+//    )
+// }
+
+const Navbar = () => {
+     return (
+      <>
+         <div>Hey I am Navbar </div>
+      </>
+     )
+}
+
+export default Navbar
