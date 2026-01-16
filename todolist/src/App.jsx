69bf4c3c49cc9b3b75ff8396af9f1e566c36f592
shubhg17 <shubamghai116@gmail.com>
2026-01-17 02:45:32 +0530

    Added CSS and created basic component for Todo

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -6,7 +6,33 @@ function App() {
   return (
     <>
       <Navbar/>
-      <h1 className="heading">TODO LIST REACT APP</h1>
+      <div className="outerdiv">
+         <div className="innerdiv">
+          <h1 className="todoheading">iTask- Manage your todos at one place</h1>
+          <h2 className="h2space">Add a Todo</h2>
+
+          <div className="div1">
+            <input type="text" className="inputbox"></input>
+           <button className="todo-btn">Save</button>
+          </div>
+         
+
+          <div className="div2">
+          <input type="checkbox"></input>
+          <p>Show Finished</p>
+          </div>
+
+           <h2 className="h2space">Your Todos</h2>
+
+          <div className="div3">
+             <input type="checkbox"></input>
+             <p>Your Task </p>
+             <button className="todo-btn">Edit</button>
+             <button className="todo-btn">Delete</button>
+          </div>
+      </div>
+      </div>
+      
     </>
   )
 }
--- todolist/src/components/Navbar.jsx
+++ todolist/src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from  "react"
 
 // function Card() {
 //    return (
@@ -10,7 +9,18 @@ import React from  "react"
 const Navbar = () => {
      return (
       <>
-         <div>Hey I am Navbar </div>
+        <div className="navbar">
+         <div className="logo">
+            <span className="itask">iTask</span>
+         </div>
+         
+         {/* isko bhi flex krdenge as isme para ha and voh bydefault ek block element ha isliye flex krna padega is wale div ko bhi  */}
+         <div className="menu">
+           <p className="m1">Home</p>
+           <p className="m2">Your Tasks</p>
+         </div>
+
+        </div>
       </>
      )
 }
