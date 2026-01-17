c066255231380ed42baf99fe21a58b9b12924775
shubhg17 <shubamghai116@gmail.com>
2026-01-17 19:17:18 +0530

    Added react logic and built todo

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,37 +1,90 @@
 
 import './App.css'
+import {useState} from "react"
 import Navbar from './components/Navbar'
 function App() {
+
+   const [text  , setText] = useState("");
+   const [listData , setlistData] = useState([])
+
+  
+  const handleAdd = ()=> {
+    //setlistData me sare list data ki copy banaleni ha ... spread operator but with this chlega but asynchronous tareeke se chlega ekdum add nhi hoga slow add hoga agar ese likhenge second way callback function ka use krke variable me hold karake return krenge toh voh synchronously mtlb fast ekdum se show hoga sahi dono hai 
+      // setlistData([...listData , text])
+      setlistData((listData)=> {
+          const updatedList = [...listData , text];
+          console.log(updatedList)
+          setText("")
+          return updatedList;
+      })
+  }
+
+  // const handleEdit = ()=> {
+     
+  // }
+
+  const handleDelete = (i)=> {
+        //basically isme hum listke data ko delete krenge unke index ke basis pe voh ha i uske liye hum use krenge filter method ki filter krdenge list ke data ko index ke basis pe 
+        const updatedList = listData.filter((elem , id)=> {
+            return i!=id;
+        })
+        setlistData(updatedList);
+  }
+
+  function removeAll() {
+      setlistData([])
+  }
 
   return (
     <>
       <Navbar/>
-      <div className="outerdiv">
-         <div className="innerdiv">
+    <div className="outerdiv">
           <h1 className="todoheading">iTask- Manage your todos at one place</h1>
           <h2 className="h2space">Add a Todo</h2>
 
           <div className="div1">
-            <input type="text" className="inputbox"></input>
-           <button className="todo-btn">Save</button>
+            <input 
+            type="text" 
+            placeholder = "Add Your Task"
+            className="inputbox" 
+            value={text} 
+            onChange={(e)=>setText(e.target.value)
+            }
+            ></input>
+           <button onClick = {handleAdd} className="todo-btn">Add</button>
           </div>
          
 
-          <div className="div2">
+          {/* <div className="div2">
           <input type="checkbox"></input>
           <p>Show Finished</p>
-          </div>
+          </div> */}
 
            <h2 className="h2space">Your Todos</h2>
 
-          <div className="div3">
+           {listData!=[] && listData.map((data , i)=> {
+                return (
+                  <>
+                   <p key={i}>
+                     <div className="datadiv">
+                      <div className="list-data">{data}</div>
+                      <button onClick = {()=>handleDelete(i)} className="todo-btn">Delete</button>
+                     </div>
+                   </p>
+                  </>
+                )
+           })}
+
+          {/* <div className="div3">
              <input type="checkbox"></input>
-             <p>Your Task </p>
-             <button className="todo-btn">Edit</button>
-             <button className="todo-btn">Delete</button>
-          </div>
-      </div>
-      </div>
+             <p id="todo-task"></p>
+             <button onClick = {handleEdit} className="todo-btn">Edit</button>
+             <button onClick = {handleDelete} className="todo-btn">Delete</button>
+          </div> */}
+          {listData.length>=1 && <button onClick = {removeAll} className="todo-btn">Remove All Tasks</button>}
+          
+
+    </div>
       
     </>
   )
--- todolist/src/components/Navbar.jsx
+++ todolist/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 
-// function Card() {
+// function Navbar() {
 //    return (
 //       <>
 //       </>
